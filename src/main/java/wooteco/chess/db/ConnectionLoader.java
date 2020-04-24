3b8279f1fc199da2b12c4bf37b08d3763617e293
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-24 17:16:49 +0900

    refactor : 불필요한 메소드 제거 및 네이밍 수정

--- src/main/java/wooteco/chess/db/ConnectionLoader.java
+++ src/main/java/wooteco/chess/db/ConnectionLoader.java
@@ -13,17 +13,19 @@ public class ConnectionLoader {
 	private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
 	private static final String NAME = "root";
 	private static final String PASSWORD = "ans230";
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-	public Connection load() {
+	public static Connection load() throws ClassNotFoundException, SQLException {
 		Connection con = null;
 		try {
 			Class.forName(DRIVER);
 			con = DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DB_NAME + OPTION, NAME, PASSWORD);
 		} catch (ClassNotFoundException e) {
 			System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
+			throw new ClassNotFoundException("데이터베이스에 연결할 수 없습니다.");
 		} catch (SQLException e) {
 			System.err.println("연결 오류:" + e.getMessage());
+			throw new SQLException("데이터베이스에 연결할 수 없습니다.");
 		}
 		return con;
 	}
